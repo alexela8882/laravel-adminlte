32e786496c18ff11c3a8ca7a56a647adffcdcedd
alexela8882 <alexela8882@gmail.com>
2019-05-30 14:50:28 +0800

    footer updated

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -1,7 +1,7 @@
 <footer class="main-footer">
   <div class="pull-right hidden-xs">
-    <b>Version</b> 2.4
+    <b>Version</b> 1.0
   </div>
-  <strong>Copyright &copy; <?php echo date('Y'); ?> <a href="https://addessa.com" target="_blank">Addessa Corporation</a>.</strong> All rights
+  <strong>Copyright &copy; <?php echo date('Y'); ?> <a href="https://github.com/alexela8882" target="_blank">alexela8882</a>.</strong> All rights
   reserved.
 </footer>
